969ead80ab186fa85b3927e5755e72a0862f053c
gumplunger <gumplunger@776878ea-5f69-11de-b7a8-4be610c0df87>
2009-06-25 17:05:13 +0000

    tied some screens together and did some basic intents management

--- BetterHood/src/com/lcc3710/CreateAccount1.java
+++ BetterHood/src/com/lcc3710/CreateAccount1.java
@@ -1,27 +1,57 @@
 package com.lcc3710;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class CreateAccount1 extends Activity {
+	private Intent ca1;
+	
+	Button buttonBack;
+    Button buttonForward;
+    
 	/** Called when the activity is first created. */
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch (requestCode) {
+    	case Login.REQ_CREATE_ACCOUNT:
+    		
+    		if (resultCode == RESULT_OK) {
+    			setResult(RESULT_OK, ca1);
+    			finish();
+    		}
+    		
+    		if (resultCode == RESULT_CANCELED) {
+    			//stay here, the user clicked back
+    		}
+    		
+    		break;
+    	}
+    }
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account_1);
         
-        Button buttonBack = (Button) findViewById(R.id.buttonBack);
-        Button buttonForward = (Button) findViewById(R.id.buttonForward);
+        ca1 = getIntent();
         
+        buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonForward = (Button) findViewById(R.id.buttonForward);
+        
+        /* buttonBack's click listener */
         buttonBack.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
+				setResult(RESULT_CANCELED, ca1);
+				finish();
 			}
         });
         
+        /* buttonForward's click listener */
         buttonForward.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
+				Intent ca2 = new Intent(view.getContext(), CreateAccount2.class);
+				startActivityForResult(ca2, Login.REQ_CREATE_ACCOUNT);
 			}
         });
     }
--- BetterHood/src/com/lcc3710/CreateAccount2.java
+++ BetterHood/src/com/lcc3710/CreateAccount2.java
@@ -1,19 +1,51 @@
 package com.lcc3710;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class CreateAccount2 extends Activity {
-    /** Called when the activity is first created. */
+	
+	private Intent ca2;
+	
+	private Button buttonBack;
+	private Button buttonForward;
+	
+    /** Called when the activity is first created. */	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account_2);
         
+        ca2 = getIntent();
+        
+        buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonForward = (Button) findViewById(R.id.buttonForward);
+        
         ListView listIHave = (ListView) findViewById(R.id.listIHave);
         listIHave.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, aszIHave));
+        
+        buttonBack.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setResult(RESULT_CANCELED, ca2);
+				finish();
+			}        	
+        });
+        
+        buttonForward.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO create account in database
+				// but lets pretend we already succeeded
+				setResult(RESULT_OK, ca2);
+				finish();
+			}        	
+        });
     }
     
     static final String[] aszIHave = new String[] {
