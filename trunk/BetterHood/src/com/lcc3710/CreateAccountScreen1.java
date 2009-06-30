d5f12ac34d8ec6b4df9ccb7333416a3887e16dae
gumplunger <gumplunger@776878ea-5f69-11de-b7a8-4be610c0df87>
2009-06-30 23:04:50 +0000

    polishing the first account create screen

--- trunk/BetterHood/src/com/lcc3710/CreateAccountScreen1.java
+++ trunk/BetterHood/src/com/lcc3710/CreateAccountScreen1.java
@@ -3,14 +3,30 @@ package com.lcc3710;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateAccountScreen1 extends Activity {
 	private Intent ca1;
 	
-	Button buttonBack;
-    Button buttonForward;
+	private Button buttonBack;
+	private Button buttonForward;
+    
+	private EditText editName;
+	private EditText editAddress;
+	private EditText editCommunityCode;
+	private EditText editUsername;
+	private EditText editPassword;
+	private EditText editPasswordConfirm;
+    
+	private TextView textUsernameAvailability;
+   
+	private static int[] illegalCharacters = { KeyEvent.KEYCODE_SLASH, KeyEvent.KEYCODE_SPACE, KeyEvent.KEYCODE_APOSTROPHE, KeyEvent.KEYCODE_STAR };
+    
     
 	/** Called when the activity is first created. */
     @Override
@@ -39,6 +55,14 @@ public class CreateAccountScreen1 extends Activity {
         buttonBack = (Button) findViewById(R.id.buttonBack);
         buttonForward = (Button) findViewById(R.id.buttonForward);
         
+        editName = (EditText) findViewById(R.id.editName);
+        editAddress = (EditText) findViewById(R.id.editAddress);
+        editUsername = (EditText) findViewById(R.id.editUsername);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        editPasswordConfirm = (EditText) findViewById(R.id.editPasswordConfirm);
+        
+        textUsernameAvailability = (TextView) findViewById(R.id.textUsernameAvailability);
+        
         /* buttonBack's click listener */
         buttonBack.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
@@ -54,5 +78,49 @@ public class CreateAccountScreen1 extends Activity {
 				startActivityForResult(ca2, LoginScreen.REQ_CREATE_ACCOUNT);
 			}
         });
+        
+        editUsername.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO Auto-generated method stub
+				boolean result = false;
+				
+				for (int i = 0; i < illegalCharacters.length; i++) {
+					if (keyCode == illegalCharacters[i]) {
+						result = true;
+						break;
+					}
+				}
+				
+				String tempUsername = editUsername.getText().toString().trim();
+				
+				if (tempUsername.length() > 6) {
+					textUsernameAvailability.setText("Username looks good so far...");
+				} else {
+					textUsernameAvailability.setText("Username must be at least 6 characters");
+				}
+				return result;
+			}        	
+        });
+        
+        OnKeyListener buttonKeyListener = new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {				
+				if (event.getAction() == KeyEvent.ACTION_DOWN) {
+					switch (keyCode) {										
+						case KeyEvent.KEYCODE_ENTER:
+							if (v.findFocus().getId() == buttonBack.getId()) {
+								buttonBack.performClick();
+							} 
+							if (v.findFocus().getId() == buttonForward.getId()) {
+								buttonForward.performClick();
+							}
+							return true;
+					}
+				}
+				return false;
+			}        	
+        };
+        
+        buttonBack.setOnKeyListener(buttonKeyListener);
+        buttonForward.setOnKeyListener(buttonKeyListener);
     }
 }
