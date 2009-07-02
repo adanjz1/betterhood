3a471ec77cf89267322078590e48960aad4d7c00
gumplunger <gumplunger@776878ea-5f69-11de-b7a8-4be610c0df87>
2009-07-02 18:30:40 +0000

    login works! going to make it a little more functional, but its there

--- BetterHood/src/com/lcc3710/BetterHood.java
+++ BetterHood/src/com/lcc3710/BetterHood.java
@@ -1,10 +1,24 @@
 package com.lcc3710;
 
 public class BetterHood {
+	//intent request thingies
 	public static final int REQ_LOGIN = 0;
 	public static final int REQ_CREATE_ACCOUNT = 1;
 	public static final int REQ_HOME_SCREEN = 2;
 	public static final int REQ_CREATE_EVENT = 3;
+	
+	//debug tags
+	public static final String TAG_LOGIN_SCREEN = "login";
+	public static final String TAG_CREATE_ACCOUNT_SCREEN_1 = "ca1";
+	public static final String TAG_CREATE_ACCOUNT_SCREEN_2 = "ca2";
+	public static final String TAG_LOGIN_PROCESS = "login_process";
+	
+	//extras tags
+	public static final String EXTRAS_QUERY = "query";
+	public static final String EXTRAS_WEB_RESPONSE = "webResponse";
+	public static final String EXTRAS_ERROR_MESSAGE = "error";
+	
+	public static final String ERROR_PREFIX = "Error: ";
 	
 	public static final int TOAST_TIME = 5;
 	
--- BetterHood/src/com/lcc3710/LoginScreen.java
+++ BetterHood/src/com/lcc3710/LoginScreen.java
@@ -1,6 +1,7 @@
 package com.lcc3710;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,12 +24,33 @@ public class LoginScreen extends Activity {
     	case BetterHood.REQ_LOGIN:
     		switch (resultCode) {
     		case RESULT_OK:
-    			// login succeeded, on to the main screen
-    			startHomeScreen();
+    			// see if we got a response
+    			String response;
+    			boolean loginSuccess;
+    			
+    			if ((response = data.getExtras().getString(BetterHood.EXTRAS_WEB_RESPONSE)) != null) {
+    				if (response.equals("true")) {
+    					Toast.makeText(this, "Logged in!", BetterHood.TOAST_TIME);
+    	    			startHomeScreen();
+    				} else {
+    					Toast.makeText(this, "Incorrect username/password combination. Try again!", BetterHood.TOAST_TIME).show();
+    				}
+    			} else {
+    				Toast.makeText(this, BetterHood.ERROR_PREFIX + "No server response", BetterHood.TOAST_TIME);
+    			}
+    			
+    			
     			break;
     		case RESULT_CANCELED:
-    			// login failed, try again
-    			clearPasswordField();
+    			// login failed, lets find out why
+    			Bundle extras = data.getExtras();
+    			String szErrorMessage;
+    			
+    			if ((szErrorMessage = extras.getString(BetterHood.EXTRAS_ERROR_MESSAGE)) != null) {
+    				//we have an error message
+    				Toast.makeText(this, BetterHood.ERROR_PREFIX + szErrorMessage, BetterHood.TOAST_TIME).show();
+    			}
+    			
     			break;
     		}
     		break;
@@ -68,20 +90,20 @@ public class LoginScreen extends Activity {
         
         //auto put some stuff into the fields, for debug
         if (BetterHood.DEBUG) {
-        	editUsername.setText("admin");
-        	editPassword.setText("password");
+        	editUsername.setText("Cat");
+        	editPassword.setText("Cat");
         }
         
         buttonLogIn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
 				String tempUsername = editUsername.getText().toString();
-				String tempPassword = editPassword.getText().toString();
+				String tempPassword = editPassword.getText().toString();				
+				String tempQuery = "Name=" + tempUsername + "&Password=" + tempPassword;
 				
 				if ((tempUsername.length() > 0) && (tempPassword.length() > 0)) {
 				
-					Intent inLogIn = new Intent(view.getContext(), LoginProcess.class);
-					inLogIn.putExtra("username", tempUsername);
-					inLogIn.putExtra("password", tempPassword);
+					Intent inLogIn = new Intent(view.getContext(), ConnectionResource.class);
+					inLogIn.putExtra(BetterHood.EXTRAS_QUERY, tempQuery);
 					
 					startActivityForResult(inLogIn, BetterHood.REQ_LOGIN);
 				} else {
