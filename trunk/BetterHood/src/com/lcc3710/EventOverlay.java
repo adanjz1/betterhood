177adf925fbc4302b527353bb38289f53fd0c980
gumplunger <gumplunger@776878ea-5f69-11de-b7a8-4be610c0df87>
2009-09-16 05:44:39 +0000

    Higher res icons, icon size now defined by variable, icon resizing, deleted some stuff that wasn't being used

--- trunk/BetterHood/src/com/lcc3710/EventOverlay.java
+++ trunk/BetterHood/src/com/lcc3710/EventOverlay.java
@@ -27,6 +27,8 @@ import com.google.android.maps.Overlay;
 public class EventOverlay extends Overlay {
 
 	private Bitmap bubbleIcon, shadowIcon;
+	
+	private static int iconWidth = 70;
 
 	private HomeScreen homeScreen;
 
@@ -58,8 +60,8 @@ public class EventOverlay extends Overlay {
 		this.homeScreen = map;
 		uname = name;
 		uID = ID;
-		shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
-		bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.party);
+		//shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
+		bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.balloon);
 	}
 
 	@Override
@@ -148,25 +150,29 @@ public class EventOverlay extends Overlay {
 			mapView.getProjection().toPixels(location.getPoint(), screenCoords);
 
 			if (shadow) {
+				//there's nothing in shadow.png, so im commenting this stuff out
+				
 				//  Only offset the shadow in the y-axis as the shadow is angled so the base is at x=0; 
-				canvas.drawBitmap(shadowIcon, screenCoords.x, screenCoords.y - shadowIcon.getHeight(),null);
+				//shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
+				//int tempHeight = (int)(((float)iconWidth / (float)shadowIcon.getWidth()) * (float)shadowIcon.getHeight());
+				//shadowIcon = Bitmap.createScaledBitmap(shadowIcon, iconWidth, tempHeight, true);
+				//canvas.drawBitmap(shadowIcon, screenCoords.x, screenCoords.y - shadowIcon.getHeight(),null);
 			} else {
 				if(location.getType().equals("Carpool")){
 					bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.services);
-					shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
 				}
 				if(location.getType().equals("Missing Child")){
 					bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.alert);
-					shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
 				}
 				if(location.getType().equals("Pool Party")) {
-					bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.party);
-					shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
+					bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.balloon);
 				}
 				if(location.getType().equals("Potluck")){
 					bubbleIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.charity);
-					shadowIcon = BitmapFactory.decodeResource(this.homeScreen.getResources(),R.drawable.shadow);
 				}
+				//resize based on iconWidth
+				int tempHeight = (int)(((float)iconWidth / (float)bubbleIcon.getWidth()) * (float)bubbleIcon.getHeight());
+				bubbleIcon = Bitmap.createScaledBitmap(bubbleIcon, iconWidth, tempHeight, true);
 				canvas.drawBitmap(bubbleIcon, screenCoords.x - bubbleIcon.getWidth()/2, screenCoords.y - bubbleIcon.getHeight(),null);
 			}
 		}
